da388d83b767c5923d87d1083254f3912e92e861
Супрун Сергей <4everyongg@mail.ru>
2021-09-30 14:07:36 +0400

    Add project files.

---
+++ Problem2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal input = decimal.Parse(ReadLine());
+            WriteLine("Стоимость товара без НДС: {0}", Math.Round(input * 5 / 6, 2));
+            WriteLine("Сумма НДС в чеке: {0}", Math.Round(input / 6, 2));
+            WriteLine("Сумма НДС для налоговой декларации: {0}", Math.Floor(input/6));
+        }
+    }
+}
